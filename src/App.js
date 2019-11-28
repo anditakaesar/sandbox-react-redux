a130973870bb2668d3d3ad0390078682877032d7
anditakaesar <anditakaesar@gmail.com>
2019-11-28 18:22:41 +0700

    Router demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import './App.css';
 import '../node_modules/uikit/dist/css/uikit.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import ChecklistDemo from './checklist/ChecklistDemo';
 
 class App extends React.Component {
   render() {
-    return <ChecklistDemo />
+    return (
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/pageone">Page One</Link>
+            </li>
+            <li>
+              <Link to="/pagetwo">Page Two</Link>
+            </li>
+            <li>
+              <Link to="/checklist">Checklist Demo</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <main>
+          <Switch>
+            <Route path="/pageone">
+              <PageOne />
+            </Route>
+            <Route path="/pagetwo">
+              <PageTwo />
+            </Route>
+            <Route>
+              <ChecklistDemo />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    )
+  }
+}
+
+class PageOne extends React.Component {
+  render() {
+    return (
+      <h1>A PageOne</h1>
+    )
+  }
+}
+
+class PageTwo extends React.Component {
+  render() {
+    return (
+      <h1>A PageTwo</h1>
+    )
   }
 }
 
